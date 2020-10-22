c5e8b09f7e381fca8f3639c596b2267833330830
MattiHel85 <70198997+MattiHel85@users.noreply.github.com>
2020-10-22 14:35:38 +0300

    make Box into separate file and import into App

--- boxes/src/App.js
+++ boxes/src/App.js
@@ -1,15 +1,6 @@
 import React from 'react';
 import './App.css';
-
-function Box(props){
-  return (
-    <div className="Box">
-      <h1>{props.name}</h1>
-      <p>Title: {props.title}</p>
-      <p>Age: {props.age}</p>
-    </div>   
-  )
-}
+import Box from './box';
 
 function App() {
   return (
---
+++ boxes/src/box.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function Box(props){
+  return (
+    <div className="Box">
+      <h1>{props.name}</h1>
+      <p>Title: {props.title}</p>
+      <p>Age: {props.age}</p>
+    </div>   
+  )
+}
+
+export default Box;

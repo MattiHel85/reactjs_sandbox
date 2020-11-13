40c700c0147967681f44c9bc0bf0b51048e8237f
MattiHel85 <70198997+MattiHel85@users.noreply.github.com>
2020-11-13 10:48:00 +0200

    Update app.js
    Include handlers to add/deduct likes and reset the counter

--- 3_task/src/App.js
+++ 3_task/src/App.js
@@ -1,9 +1,44 @@
+import React, { Component } from "react";
+import Header from "./Components/Header/Header";
+import Footer from "./Components/Footer/Footer";
 import './App.css';
 
-function App() {
-  return (
-    <h1>Hello World!</h1>
-  );
+class App extends Component {
+  state = {
+    likes: 0,
+  };
+  addHandler = () => {
+    this.setState({
+      likes: this.state.likes +1
+    });
+  };
+  removeHandler = () => {
+    this.state.likes === 0
+      ? this.setState({ likes: 0 })
+      : this.setState({ likes: this.state.likes -1});
+  };
+  resetHandler = () => {
+    this.setState({ likes: 0 });
+  };
+  render() {
+    let circleClass = `${
+      this.state.likes === 0 ? "" : this.state.likes % 2 === 0 ? "even":"odd"
+    } likes`;
+
+    return (
+      <>
+        <Header />
+        <main>
+          <h1 className={circleClass}>Total likes: {this.state.likes}</h1>
+          <button onClick={this.addHandler}>Add one</button>
+          <button onClick={this.removeHandler}>Delete one</button>
+          <button onClick={this.resetHandler}>Reset</button>
+        </main>
+        <Footer />
+      </>
+    );
+  }
+
 }
 
 export default App;
